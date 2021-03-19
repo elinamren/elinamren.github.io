79909763ca8708805e00f16d1216fa5a23b18f1c
Elin Amren <elin.amren@gmail.com>
2021-03-19 13:28:12 +0100

    typos and responsive background

--- src/components/Mountains.js
+++ src/components/Mountains.js
@@ -1,10 +1,14 @@
 import mountains from "../images/mountains.png";
+import mountainsSmall from "../images/mountains-small.png";
+import { useMediaQuery } from "react-responsive";
 
 const Mountains = () => {
+  const isMobile = useMediaQuery({ query: `(max-width: 960px)` });
+
   return (
     <img
       className="background-img"
-      src={mountains}
+      src={isMobile ? mountainsSmall : mountains}
       alt="green mountains in the background"
     />
   );
